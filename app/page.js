e054a86efcddf89e73e9c18e87ecb22a1a1355ca
Maru5er <marcohendradani@gmail.com>
2024-07-04 14:20:29 -0700

    navbar

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
-import Image from "next/image";
 import './globals.css'
+import Navbar from './components/navbar';
 
 export default function Home() {
   return(
-    <div className="flex justify-center items-center h-screen text-center p-4">
-      <h1 className="text-3xl md:text-5xl">Welcome to Genie! Make your story come to life</h1>
+    <div>
+      <Navbar />
+      <div className="flex-column justify-center items-center h-screen text-center p-4">
+        <h1 className="text-3xl md:text-5xl mb-4">Welcome to Genie! <n/> Make your story come to life</h1>
+        <div>
+          This is the input
+        </div>
+      </div>
     </div>
     
   );
 }
+
+
