bae1920341bd0842d293b6a5fbc334048a72fd3c
Maru5er <74164821+Maru5er@users.noreply.github.com>
2024-07-05 22:28:02 -0700

    added functional dropdown

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import {Switch, Textarea} from '@headlessui/react';
 import { useEffect, useState } from 'react';
 import { ImageGallery } from 'react-image-grid-gallery';
 import { RingLoader } from 'react-spinners';
+import { Select, Label, Field, Description } from '@headlessui/react';
 
 
 
@@ -14,18 +15,26 @@ export default function Home() {
   const [loaded, setLoaded] = useState(false);
   const [imagesArray, setImagesArray] = useState([]);
   const [text, setText] = useState('');
+  const [style, setStyle] = useState('Claymation');
+  const [imageNumber, setImageNumber] = useState('3');
 
   async function getImages() {
+    if (text == '') {
+      alert('Textarea is empty');
+      return;
+    }
     console.log(process.env.NEXT_PUBLIC_SERVER_HOST);
     setGenerated(true);
     setLoaded(false);
     try {
+      var input = text;
+      input += `Generate ${imageNumber} images in ${style} style`;
       const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_HOST}/stream`, {
         method: 'POST',
         headers: {
           'Content-Type' : 'application/json',
         },
-        body : JSON.stringify({'story' : text})
+        body : JSON.stringify({'story' : input})
       });
       
       const data = await res.json();
@@ -68,25 +77,54 @@ export default function Home() {
 
         <div className="transition-opacity duration-300 ease-in-out">
           {enabled ? (
-            <div className='h-32 w-full'>
+            <div className='h-32 w-full mb-40'>
               <label htmlFor="audioInput" className="block text-lg mt-4">Audio Input:</label>
               <input type="file" id="audioInput" accept="audio/*" className="mt-2 p-2 border border-gray-300 rounded"/>
             </div>
           ) : (
-            <div className='h-32 w-full md:w-96'>
+            <div className='h-32 w-full md:w-96 mb-40'>
               <label htmlFor="textInput" className="block text-lg mt-4">Text Input:</label>
               <textarea value = {text} style={{resize:'none', width:'80vw', marginLeft:'10vw'}}id="textInput" rows="8" className="absolute left-0 mt-2 p-2 border-4 border-gray-300 rounded w-4/5 text-black"
                         onChange={(e) => setText(e.target.value)}></textarea>
             </div>
           )}
         </div>
-        <button className='mt-40 border-2 border-gray-300 p-2 hover:bg-white hover:text-black'
-          onClick={getImages}
-        >
-          Generate
-        </button>
+        <div className='flex flex-row justify-center'>
+        <Field>
+            <Label>Select the style for the image</Label>
+            <Select value={style} className='border-2 m-2 p-2 data-[hover]:shadow data-[focus]:bg-gray-500' aria-label='Style'
+                    onChange={(e) => setStyle(e.target.value)}>
+              <option value='Claymation'>Claymation</option>
+              <option value='Gothic'>Gothic</option>
+              <option value='Storybook Illustration'>Storybook</option>
+              <option value='Realistic'>Realistic</option>
+              <option value='Digital paianting'>Digital art</option>
+              <option value='Crayon drawing'>Crayon</option>
+              <option value="Children's Drawing">Children Drawing</option>
+            </Select>
+          </Field>
+          <button className='m-2 border-2 mh-4 border-gray-300 p-2 hover:bg-white hover:text-black'
+            onClick={getImages}
+          >
+            Generate
+          </button>
+          <Field>
+            <Label>Number of images</Label>
+            <Select value={imageNumber} className='border-2 m-2 p-2 data-[hover]:shadow data-[focus]:bg-gray-500' aria-label='Style'
+                    onChange={(e) => setImageNumber(e.target.value)}>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+            </Select>
+          </Field>
+          
+          
+        </div>
         
-        <div className="mt-10">
+        
+        <div className="flex justify-center items-center mt-10">
           {loaded ? (
               <ImageGallery 
               imagesInfoArray={imagesArray}
@@ -95,12 +133,14 @@ export default function Home() {
               gapSize={24}
             />
           ) : (
-            <div >
-              <RingLoader 
-                color='white'
+            <div className='flex flex-col justify-center items-center'>
+              <h3 className={`${generated ? 'opacity-100' : 'opacity-0'} text-sm m-4`}>Generating your image. Image may take several minutes to generate...</h3>
+              <RingLoader
+                color='gray'
                 loading={generated}
                 aria-label="Loading Spinner"
                 data-testid="loader"
+                size={100}
               />
             </div>
           )}
